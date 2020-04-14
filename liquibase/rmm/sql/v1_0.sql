cf7bf007a1471916869721d0601e279c1aa0d157
Adam Kimmel <adampkimmel@gmail.com>
2020-04-14 14:16:15 -0400

    RMSL-48 Update scripts (#34)

--- liquibase/rmm/sql/v1_0.sql
+++ liquibase/rmm/sql/v1_0.sql
@@ -5,13 +5,16 @@ USE rmm;
 
 CREATE TABLE records (
     guide varchar(255) primary key,
+    recordSystemGuide varchar(255) NOT NULL,
     ruleId int NOT NULL,
     rcsId int NOT NULL,
     decisionDate date NOT NULL,
     documentCreateDate date NOT NULL,
-    dispositionDate date NOT NULL
+    dispositionDate date NOT NULL,
+    dispositionAppliedBy varchar(255)
 );
 
+CREATE INDEX rsguide ON records (recordSystemGuide);
 CREATE INDEX rule ON records (ruleId);
 CREATE INDEX rcs ON records (rcsId);
 CREATE INDEX disposition ON records (dispositionDate);
