f27a40f024a051d1a8a93693b522eaa558497cec
Steve McFall <smcfall215@gmail.com>
2020-04-15 09:07:22 -0400

    RMSL-67 test global state

--- rms-frontend/libs/core/src/lib/+state/global/global.state.spec.ts
+++ rms-frontend/libs/core/src/lib/+state/global/global.state.spec.ts
@@ -1,24 +1,28 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { NgxsModule, Store } from '@ngxs/store';
 import { GlobalState, GlobalStateModel } from './global.state';
-import { GlobalAction } from './global.actions';
+import { SetTheme } from './global.actions';
+
+export const DEFAULT_STATE = {
+  global: {
+    theme: 'light'
+  }
+};
 
 describe('Global store', () => {
   let store: Store;
-  beforeEach(async(() => {
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NgxsModule.forRoot([GlobalState])]
-    }).compileComponents();
-    store = TestBed.get(Store);
-  }));
-
+    });
+    store = TestBed.inject(Store);
+    store.reset(DEFAULT_STATE);
+  });
   it('should create an action and add an item', () => {
-    const expected: GlobalStateModel = {
-      items: ['item-1']
-    };
-    store.dispatch(new GlobalAction('item-1'));
-    const actual = store.selectSnapshot(GlobalState.getState);
+    const expected = 'newTheme';
+    store.dispatch(new SetTheme('newTheme'));
+    const actual = store.selectSnapshot(GlobalState.getTheme);
     expect(actual).toEqual(expected);
   });
-
 });
