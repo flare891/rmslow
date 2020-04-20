05335cf5fc1891105d79c4ff8be1f2da1339b488
saepark90 <saepark90@gmail.com>
2020-04-20 10:21:13 -0400

    Fix missing modules for lazy load page (#47)

--- rms-frontend/apps/lazy-load/src/app/app.module.ts
+++ rms-frontend/apps/lazy-load/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '@rms-frontend/core';
 
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CoreModule
   ],
   providers: [],
