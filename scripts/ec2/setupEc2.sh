df61a5b5531d6022aa22ab3af8c111506ab3979e
Adam Kimmel <adampkimmel@gmail.com>
2020-04-28 08:24:46 -0400

    RMSL-93 Move to two seperate NiFi instances (#68)
    * RMSL-93 Move to two seperate NiFi instances
    
    * Fixes

--- scripts/ec2/setupEc2.sh
+++ scripts/ec2/setupEc2.sh
@@ -34,15 +34,20 @@ rm nifi-1.11.4-bin.tar.gz
 mv nifi-1.11.4 nifi
 sed -i 's/-Xms512m/-Xms2048m/g' nifi/conf/bootstrap.conf
 sed -i 's/-Xmx512m/-Xmx2048m/g' nifi/conf/bootstrap.conf
-sed -i 's/nifi.remote.input.host=/nifi.remote.input.host=localhost/g' nifi/conf/nifi.properties
-sed -i 's/nifi.remote.input.socket.port=/nifi.remote.input.socket.port=10000/g' nifi/conf/nifi.properties
-sed -i 's/nifi.remote.input.http.enabled=false/nifi.remote.input.http.enabled=true/g' nifi/conf/nifi.properties
 cp rmslow/NiFi/flows/flow.xml.gz nifi/conf/flow.xml.gz
 pushd rmslow/NiFi
 mvn clean install
 cp rms-custom-nar/target/rms-custom-processors-nar-1.0-SNAPSHOT.nar /rms/nifi/lib/rms-custom-processors-nar-1.0-SNAPSHOT.nar
 popd
 mkdir /rms/nifi-input
+cp -r nifi nifi2
+sed -i 's/nifi.web.http.port=8080/nifi.web.http.port=9090/g' nifi2/conf/nifi.properties
+sed -i 's/nifi.remote.input.host=/nifi.remote.input.host=localhost/g' nifi2/conf/nifi.properties
+sed -i 's/nifi.remote.input.socket.port=/nifi.remote.input.socket.port=10000/g' nifi2/conf/nifi.properties
+sed -i 's/nifi.remote.input.http.enabled=false/nifi.remote.input.http.enabled=true/g' nifi2/conf/nifi.properties
+cp rmslow/NiFi/flows/flow2.xml.gz nifi2/conf/flow.xml.gz
+bash nifi2/bin/nifi.sh start
+sleep 10
 bash nifi/bin/nifi.sh start
 
 # Install MySQL
@@ -63,6 +68,7 @@ popd
 cd /rms
 crontab -l > mycron
 echo "*/10 * * * * aws s3 cp /rms/nifi/conf/flow.xml.gz s3://rmslowdeployment/flow.xml.gz" >> mycron
+echo "*/10 * * * * aws s3 cp /rms/nifi2/conf/flow.xml.gz s3://rmslowdeployment/flow2.xml.gz" >> mycron
 echo "0 * * * * mysql -u root -e 'use rmm; call populate_record_metrics();'" >> mycron
 echo "1 * * * * mysql -u root -e 'use rmm; call populate_record_system_metrics();'" >> mycron
 echo "2 * * * * mysql -u root -e 'use rmm; call populate_rcs_metrics();'" >> mycron
