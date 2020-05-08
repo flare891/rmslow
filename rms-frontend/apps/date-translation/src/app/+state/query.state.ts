3c8d8fe75aed7d640fd5777dd722855e0da5c653
flare891 <63003975+flare891@users.noreply.github.com>
2020-05-08 11:17:41 -0400

    RMSL-28 (#83)
    * RMSL-28 date manip
    
    * RMSL-28 a bunch of state changes
    
    * RMSL-28 more state goodness
    
    * RMSL-28 a bunch more updates
    
    * RMSL-28 committing date range -> lib code
    
    * RMSL-28 adding home link
    
    * RMSL-28 removing unnecessary state code

--- rms-frontend/apps/date-translation/src/app/+state/query.state.ts
+++ rms-frontend/apps/date-translation/src/app/+state/query.state.ts
@@ -4,19 +4,11 @@ import { Injectable } from '@angular/core';
 import { ImmutableContext } from '@ngxs-labs/immer-adapter';
 
 export interface QueryStateModel {
-  startQuery: string;
-  startDate: Date;
-  endDate: Date;
-  queryType: string;
   endQuery: string;
 }
 @State<QueryStateModel>({
   name: 'query',
   defaults: {
-    startQuery: "",
-    startDate: null,
-    endDate: null,
-    queryType: "",
     endQuery: ""
   }
 })
@@ -24,58 +16,9 @@ export interface QueryStateModel {
 export class QueryState {
 
   @Selector()
-  public static startQuery(state: QueryStateModel) {
-    return state.startQuery;
-  }
-
-  @Selector()
-  public static startDate(state: QueryStateModel) {
-    return state.startDate;
-  }
-
-  @Selector()
-  public static endDate(state: QueryStateModel) {
-    return state.endDate;
-  }
-
-  @Selector()
-  public static queryType(state: QueryStateModel) {
-    return state.queryType;
-  }
-
-  @Selector()
   public static endQuery(state: QueryStateModel) {
     return state.endQuery;
   }
-
-
-  // @Action(updateStartQuery)
-  // public updateStartQuery(ctx: StateContext<QueryStateModel>, { payload }: updateStartQuery) {
-  //   const stateModel = ctx.getState();
-  //   stateModel.startQuery = payload;
-  //   ctx.setState(stateModel);
-  // }
-
-  // @Action(updateStartDate)
-  // public updateStartDate(ctx: StateContext<QueryStateModel>, { payload }: updateStartDate) {
-  //   const stateModel = ctx.getState();
-  //   stateModel.startDate = payload;
-  //   ctx.setState(stateModel);
-  // }
-
-  // @Action(updateEndDate)
-  // public updateEndDate(ctx: StateContext<QueryStateModel>, { payload }: updateEndDate) {
-  //   const stateModel = ctx.getState();
-  //   stateModel.endDate = payload;
-  //   ctx.setState(stateModel);
-  // }
-
-  // @Action(updateQueryType)
-  // public updateQueryType(ctx: StateContext<QueryStateModel>, { payload }: updateQueryType) {
-  //   const stateModel = ctx.getState();
-  //   stateModel.queryType = payload;
-  //   ctx.setState(stateModel);
-  // }
 
   @Action(updateEndQuery)
   @ImmutableContext()
